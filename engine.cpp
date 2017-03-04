441a6a92eda66bb9c74ad0530007d48c3c43e670
MiguelSilva96 <a74601@alunos.uminho.pt>
2017-03-04 03:22:13 +0000

    Generator only needs the cone, input validations and some eventual touches, class Vertex available for engine

---
+++ engine.cpp
@@ -0,0 +1,13 @@
+/**
+    Practical Assignment Computer Graphics
+    engine.cpp
+    Purpose: Display figures on the screen as instructed on input file
+
+    @author Jose Silva, Joao Coelho, Luís Fernandes, Pedro Cunha
+    @version 1.0
+*/
+
+class Vertex {
+    public:
+        const int x, y, z;
+};
